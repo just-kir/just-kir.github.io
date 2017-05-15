cebbe58dfbacf374cb7c264497eaa3545fcbb1ed
Kirill <dontwritehim@gmail.com>
2017-05-15 19:28:16 +0300

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -1,4 +1,4 @@
 var  codes_big = ['messwiththebest', 'oldfriend']
-var  codes_medium = ['solved', 'simargl', 'turgor179']
+var  codes_medium = ['solved', 'simargl', 'turgor179', 'LevPrav']
 var  codes_small = ['hypothesis']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
