d05caab66b2bb316b9abb59d052de6349ea9e5ea
Kirill <dontwritehim@gmail.com>
2017-05-24 16:24:43 +0300

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -1,4 +1,5 @@
 var  codes_big = ['messwiththebest', 'oldfriend']
 var  codes_medium = ['solved', 'simargl', 'turgor179', 'LevPrav']
 var  codes_small = ['hypothesis']
+var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
