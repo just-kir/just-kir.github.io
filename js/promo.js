f40e440bc2364fe6dcdafe348fad4e776423f1dc
Kirill <dontwritehim@gmail.com>
2017-06-29 14:52:48 +0300

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -1,4 +1,4 @@
-var  codes_big = ['messwiththebest', 'oldfriend']
+var  codes_big = ['messwiththebest', 'oldfriend', 'akonowalow']
 var  codes_medium = ['solved', 'simargl', 'turgor179', 'LevPrav']
 var  codes_small = ['hypothesis']
 var  prepay = ['prepay']
