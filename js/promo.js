1915c4c0fe473b176647618ea028454739db9389
ildar-khisambeev <i_khisambeev@mail.ru>
2017-08-02 14:26:26 +0300

    left is right

--- js/promo.js
+++ js/promo.js
@@ -1,5 +1,5 @@
 var  codes_big = ['messwiththebest', 'oldfriend', 'akonowalow']
-var  codes_medium = ['solved', 'simargl', 'turgor179', 'LevPrav']
+var  codes_medium = ['solved', 'simargl', 'turgor179', 'LevPrav', 'levprav']
 var  codes_small = ['hypothesis']
 var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
