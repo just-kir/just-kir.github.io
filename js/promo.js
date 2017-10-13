de6142b477f8563decf4ddc95cdb1f2e9259f39a
Kirill <dontwritehim@gmail.com>
2017-10-13 14:43:19 +0300

    Update promo.js

--- js/promo.js
+++ js/promo.js
@@ -1,5 +1,5 @@
 var  codes_big = ['messwiththebest', '4seasons']
-var  codes_medium = ['solved', 'LevPrav', 'levprav']
+var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva']
 var  codes_small = ['hypothesis']
 var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
