12b86b406a9ace147dbaa79be4c0a722467bf054
ildar-khisambeev <i.khisambeev@gmail.com>
2017-10-17 22:45:16 +0300

    for letovo

--- js/promo.js
+++ js/promo.js
@@ -1,5 +1,5 @@
 var  codes_big = ['messwiththebest', '4seasons']
 var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva', 'temytch']
-var  codes_small = ['hypothesis']
+var  codes_small = ['hypothesis','davinci']
 var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
