586e7e6c971f4377bb18b0104bc5fc10f631944b
ildar-khisambeev <i.khisambeev@gmail.com>
2018-03-16 14:12:53 +0300

    promo for olympians

--- js/promo.js
+++ js/promo.js
@@ -1,4 +1,4 @@
-var  codes_big = ['messwiththebest', '4seasons', 'itedumipt']
+var  codes_big = ['messwiththebest', '4seasons', 'itedumipt', 'olympian']
 var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva', 'temytch']
 var  codes_small = ['hypothesis','davinci']
 var  prepay = ['prepay']
