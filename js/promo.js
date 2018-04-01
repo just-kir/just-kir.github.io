22eafde42c44506dd6ebc3acb61861b44e36344a
ildar-khisambeev <i.khisambeev@gmail.com>
2018-04-01 22:25:16 +0300

    petrenko promo

--- js/promo.js
+++ js/promo.js
@@ -1,4 +1,4 @@
-var  codes_big = ['messwiththebest', '4seasons', 'itedumipt', 'olympian', 'denka-2003', 'mail-popova']
+var  codes_big = ['messwiththebest', '4seasons', 'itedumipt', 'olympian', 'denka-2003', 'mail-popova', 'petrenkoonelove']
 var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva', 'temytch']
 var  codes_small = ['hypothesis','davinci']
 var  prepay = ['prepay']
