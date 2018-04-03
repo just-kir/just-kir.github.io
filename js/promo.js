d084e6acf622571b5eaaf695bdfeba7ad7ff74c0
ildar-khisambeev <i.khisambeev@gmail.com>
2018-04-03 18:40:59 +0300

    castlequiz promo

--- js/promo.js
+++ js/promo.js
@@ -1,5 +1,5 @@
 var  codes_big = ['messwiththebest', '4seasons', 'itedumipt', 'olympian', 'denka-2003', 'mail-popova', 'petrenkoonelove']
-var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva', 'temytch']
+var  codes_medium = ['solved', 'LevPrav', 'levprav', 'vital', 'olva', 'temytch', 'castlequiz']
 var  codes_small = ['hypothesis','davinci']
 var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
