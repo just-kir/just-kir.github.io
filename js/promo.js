faa588bbce4a67376ee31cf81f151dadf226979e
ildar-khisambeev <i.khisambeev@gmail.com>
2018-06-12 18:11:19 +0300

    apava postpay

--- js/promo.js
+++ js/promo.js
@@ -1,5 +1,5 @@
 var  codes_big = ['messwiththebest', 'perennial']
 var  codes_medium = ['akrobat76', 'konowalow']
-var  codes_small = ['hypothesis']
+var  codes_small = ['hypothesis', 'apava']
 var  prepay = ['prepay']
 // Поздравляю! Ты добрался до скидок. messwiththebest -- твой выбор.
